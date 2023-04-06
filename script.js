c67c4772a3fe1d3df0b675d2dd014c71d181a28a
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-06 00:16:34 -0400

    add js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function Auto(maker,model,year){
     this.year = year;
 }
 
+let AutosSaved = [];
+let maker;
+let model;
+let year;
+function saveACar(){
+    maker = document.getElementById("maker").value;
+    model = document.getElementById("model").value;
+    year = document.getElementById("year").value;
+    let vehNew = new Auto(maker,model,year);
+    AutosSaved.push(vehNew);
+}
+function showMe(){
+    alert(AutosSaved);
+}
